1dedaf743fb1384544834a2dcb160532b37cfdb7
杨逸 <huisama@outlook.com>
2018-02-26 10:32:18 +0800

    0.2.0 (2018-02-26)
    # 0.2.0 (2018-02-26)
    
    ## 错误修复
    
    1. `SimpleComError` 不再继承`Error`
    
    ## 新增功能
    
    2. `SimpleComError`添加`request`属性.

--- src/simple-com-error.ts
+++ src/simple-com-error.ts
@@ -1,11 +1,14 @@
+import { SimpleComRequest } from './simple-com-request';
+
 /**
  * 用于表示简单通信的异常对象.
  * 
  * @export
  * @class SimpleComError
- * @extends {Error}
  */
-export class SimpleComError extends Error {
+export class SimpleComError {
+    message: string;
+    request: SimpleComRequest;
 
     /**
      * ctr
@@ -13,7 +16,8 @@ export class SimpleComError extends Error {
      * @param {(string | { message: string })} raw 
      * @memberof SimpleComError
      */
-    constructor(public raw: string | { message: string }) {
-        super(typeof (raw) === 'string' ? raw : raw.message);
+    constructor(public raw: string | { message: string }, req: SimpleComRequest) {
+        this.message = typeof (raw) === 'string' ? raw : raw.message;
+        this.request = req;
     }
 }
